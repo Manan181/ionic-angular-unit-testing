f5ceb711242e896c5a383fd662eddd0ec51c8977
Manan Sonani <manan.s@biztechcs.com>
2023-11-06 15:40:18 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'biz-tests-demo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
